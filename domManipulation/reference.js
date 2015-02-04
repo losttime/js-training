cee21c2fffcff3c10844645f8cf475ed4d600a8b
David Cox <losttime.shuffle@gmail.com>
2015-02-04 12:23:41 -0700

    Flesh out the subject outline

--- domManipulation/reference.js
+++ domManipulation/reference.js
@@ -1,46 +1,99 @@
-// Take a look at the HTML
-// Note there is a HEAD and a BODY
-// The HEAD contains information about the page, as well as declaring needed resources
-// The BODY contains the page content
+// Review HTML format
+// <script> in <head>
+// <button> and <div> in <body>
 
-// Script in HEAD that executes immediately
-
-var triggerElement = document.querySelector('button');
-triggerElement.addEventListener('click', function(event) {
-	console.log('event', event);
-});
+var buttons = document.getElementsByTagName('button');
+console.log(buttons);
+console.log(buttons[0]);  // undefined
 
 
-// Script in HEAD that waits for pageload
+// JavaScript runs as it loads
+// 1. Browser pulls down the HTML
+// 2. Sees reference to JS as it loads the HTML
+// 3. Downloads the JS
+// 4. Loads the JS
+//
+// I don't know why the HTML gets lapped by the JS
 
-document.addEventListener( "DOMContentLoaded", kickoff, false );
+
+// Try sleeping before running
+
+setTimeout(function() {
+	var buttons = document.getElementsByTagName('button');
+	console.log(buttons[0]);
+}, 1000);
+
+
+// Why did logging buttons work, but not a specific button?
+// - When logging the array, the array exists, and gets modified just before logging.
+// - When logging a specific item, the item is asked for before it exists, so undefined is returned
+//     and undefined is logged.
+//
+// Tricky!
+
+
+// Getting around page load timing issues
+// - Put your JS in a function that gets run after an event happens
+// - Put your <script> at the end of the <body>
+
+// Script in <head> that waits for pageload
+
+document.addEventListener('DOMContentLoaded', kickoff, false);
 
 var kickoff = function() {
-	var triggerElement = document.querySelector('button');
-	triggerElement.addEventListener('click', function(event) {
-		console.log('event', event);
-	});
+	var buttons = document.getElementsByTagName('button');
+	console.log(buttons[0]);
 };
 
+// Script and end of <body>
 
-// Script in the BODY
-
-var triggerElement = document.querySelector('button');
-triggerElement.addEventListener('click', function(event) {
-	console.log('event', event);
-
-	// Put a number in the content DIV when the button is clicked
-
-	var contentContainer = document.querySelector('div');
-
-	var p = document.createElement('p');
-	p.textContent = '1';
-
-	contentContainer.appendChild(p);
-});
+var buttons = document.getElementsByTagName('button');
+console.log(buttons[0]);
 
 
-// What happens when you click the button multiple times?
+// Other methods of getting elements
+
+var button = document.getElementById('runnerButton');
+console.log(button);
 
 
-// Make the number increment
+// Add jQuery
+// <script src="https://code.jquery.com/jquery-2.1.3.js"></script>
+var buttons = $('button');
+console.log(buttons);
+
+
+var button = document.querySelector('button');
+console.log(button);
+
+
+var buttons = document.querySelectorAll('button');
+console.log(buttons);
+
+
+// Now that we have an element . . .
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement
+
+// Modify the style - it's CSS
+button.style.color = 'red';
+button.style.backgroundColor = '#bada55';
+button.style.fontSize = '2em';
+button.style.position = 'relative';
+button.style.left = '100px';
+
+// Modify the DOM itself
+button.textContent = 'Click Me!';
+
+var parent = button.parentNode;
+parent.removeChild(button);
+
+var paragraph = document.createElement('');
+paragraph.textContent = '1';
+
+var container = document.querySelector('div');
+container.appendChild(paragraph);
+
+
+// Put content in the container when the user clicks the button.
+
+// In the container, display the number of times the user has clicked the button.
